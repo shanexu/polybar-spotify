47c38eac44c02c6b3752c8634994f89c669bf7b0
Shane Xu <xusheng0711@gmail.com>
2022-11-06 20:31:23 +0800

    refine

--- get_spotify_status.sh
+++ get_spotify_status.sh
@@ -9,8 +9,17 @@ QQ_MUSIC_PID=$(pgrep -f '/usr/lib/electron/electron --enable-crashpad /usr/lib/q
 PLAYER="chromium.instance${QQ_MUSIC_PID}"
 # PLAYER=playerctld
 
-if [ -z $QQ_MUSIC_PID ]; then
-    echo '%{T3}鈴%{T-}'
+# $1 module, $2 hook
+update_module_hooks() {
+    polybar-msg action "#$1.hook.$2" 1>/dev/null 2>&1
+}
+
+if [ -z "$QQ_MUSIC_PID" ]; then
+    # echo '%{T3}鈴%{T-}'
+    echo ''
+    update_module_hooks spotify-prev 1
+    update_module_hooks spotify-next 1
+    update_module_hooks spotify-play-pause 2
     exit
 fi
 
@@ -26,16 +35,16 @@ fi
 FORMAT="{{ title }} - {{ artist }}"
 
 # The name of polybar bar which houses the main spotify module and the control modules.
-PARENT_BAR="topbar"
-PARENT_BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
+# PARENT_BAR="topbar"
+# PARENT_BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
 
 # Sends $2 as message to all polybar PIDs that are part of $1
-update_hooks() {
-    while IFS= read -r id
-    do
-        polybar-msg -p "$id" hook spotify-play-pause $2 1>/dev/null 2>&1
-    done < <(echo "$1")
-}
+# update_hooks() {
+#     while IFS= read -r id
+#     do
+#         polybar-msg -p "$id" action "#spotify-play-pause.hook.$2" 1>/dev/null 2>&1
+#     done < <(echo "$1")
+# }
 
 PLAYERCTL_STATUS=$(playerctl --player=$PLAYER status 2>/dev/null)
 EXIT_CODE=$?
@@ -48,16 +57,25 @@ fi
 
 if [ "$1" == "--status" ]; then
     echo "$STATUS"
+    # echo ''
 else
     if [ "$STATUS" = "Stopped" ]; then
         echo "No music is playing"
     elif [ "$STATUS" = "Paused"  ]; then
-        update_hooks "$PARENT_BAR_PID" 2
+        update_module_hooks spotify-prev 0
+        update_module_hooks spotify-next 0
+        update_module_hooks spotify-play-pause 1
         playerctl --player=$PLAYER metadata --format "$FORMAT"
     elif [ "$STATUS" = '%{T3}鈴%{T-}'  ]; then
+        update_module_hooks spotify-prev 0
+        update_module_hooks spotify-next 0
+        update_module_hooks spotify-play-pause 1
         echo "$STATUS"
+        # echo ''
     else
-        update_hooks "$PARENT_BAR_PID" 1
+        update_module_hooks spotify-prev 0
+        update_module_hooks spotify-next 0
+        update_module_hooks spotify-play-pause 0
         playerctl --player=$PLAYER metadata --format "$FORMAT"
     fi
 fi
--- scroll_spotify_status.sh
+++ scroll_spotify_status.sh
@@ -2,8 +2,9 @@
 
 # see man zscroll for documentation of the following parameters
 zscroll -l 30 \
+        -U 1 \
         --scroll-padding "  " \
-        --delay 0.2 \
+        --delay 0.4 \
         --match-command "`dirname $0`/get_spotify_status.sh --status" \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
